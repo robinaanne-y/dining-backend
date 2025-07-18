be87941d65493ef452c609450294dfe7215ae3fb
Robina Anne <robinaanne93@gmail.com>
2025-07-18 22:06:29 +0800

    added negative test

--- tests/Feature/UserLoginTest.php
+++ tests/Feature/UserLoginTest.php
@@ -10,7 +10,7 @@ use Tests\TestCase;
 class UserLoginTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
-    
+
     /** @test */
     public function user_can_login_with_valid_credentials(): void
     {
@@ -26,4 +26,20 @@ class UserLoginTest extends TestCase
         $this->assertArrayHasKey('access_token', $response->json());
     }
 
+    /** @test */
+    public function user_cannot_login_with_invalid_credentials(): void
+    {
+        $user = User::factory()->create([
+            'password' => bcrypt('password'),
+        ]);
+
+        $response = $this->post('/api/login', [
+            'email' => $user->email,
+            'password' => 'wrong-password',
+        ]);
+
+        $this->assertArrayNotHasKey('access_token', $response->json());
+        $response->assertStatus(401);
+        $response->assertJson(['message' => 'Invalid credentials']);
+    }
 }
